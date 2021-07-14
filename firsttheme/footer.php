01d73c09113492a1a9760ee0d5a3a78801876fb8
kjhill1 <72063861+kjhill1@users.noreply.github.com>
2021-07-14 14:46:19 -0700

    Olga Please check.
    Thank you!
    
    I was wokring on the thumbnail with index & functions.

---
+++ firsttheme/footer.php
@@ -0,0 +1,15 @@
+
+<footer>
+    <div class="copyright">
+        <ul>
+            <li>Copyright <?php echo date("Y") ;?></li>
+            <li>All Rights Reserved</li>
+            <li><a href="">Terms of Use</a></li>
+            <li><a href="">Kelly with WordPress</a></li>        
+        </ul>  
+    </div>
+    <!-- closes copyright div  -->
+</footer>
+
+    </body>
+</html>
