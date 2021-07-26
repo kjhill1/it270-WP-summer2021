2ac9f2cfbe0a71e263ad8699cccb150a16c30d5e
kjhill1 <72063861+kjhill1@users.noreply.github.com>
2021-07-26 10:26:06 -0700

    Olga Check - Body classes
    My Body tags don't look like yours.....?

--- functions.php
+++ functions.php
@@ -11,12 +11,12 @@ set_post_thumbnail_size(150, 150);
 
 add_theme_support('post-thumbnails') ;
 
-// //Page Slug Body Class
-// function add_slug_body_class( $classes ) {
-//     global $post;
-//     if ( isset( $post ) ) {
-//     $classes[] = $post->post_type . '-' . $post->post_name;
-//     }
-//     return $classes;
-//     }
-//     add_filter( 'body_class', 'add_slug_body_class' );
+//Page Slug Body Class
+function add_slug_body_class( $classes ) {
+    global $post;
+    if ( isset( $post ) ) {
+    $classes[] = $post->post_type . '-' . $post->post_name;
+    }
+    return $classes;
+    }
+    add_filter( 'body_class', 'add_slug_body_class' );
